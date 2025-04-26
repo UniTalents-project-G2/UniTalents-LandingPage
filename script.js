e784a8891588975a388cc6b2fcd6d72c7e69be27
MrAndres08DV <andrescoca0405@gmail.com>
2025-04-25 19:11:38 -0500

    feat: Footer added and responsive

---
+++ script.js
@@ -0,0 +1,12 @@
+//MENU HAMBURGUESA
+
+const menu = document.querySelector(".menu-horizontal");
+const openMenuBtn = document.querySelector(".open-menu");
+
+//Funcion
+
+function toggleMenu() {
+  menu.classList.toggle("menu_opened");
+}
+
+openMenuBtn.addEventListener("click", toggleMenu);
